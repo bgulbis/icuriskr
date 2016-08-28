f039b92842739b3d509ac048e14cbde326512b6c
Brian Gulbis <brian.gulbis@gmail.com>
2016-08-28 15:05:08 -0500

    add remaining aps variables except oxygen

--- R/class.R
+++ R/class.R
@@ -64,6 +64,86 @@ as.hr <- function(x) {
     x
 }
 
+#' @rdname riskr_class
+#' @export
+as.rr <- function(x) {
+    if (missing(x)) x <- numeric()
+    if (is.rr(x)) return(x)
+    after <- match("rr", class(x), nomatch = 0L)
+    class(x) <- append(class(x), "rr", after = after)
+    x
+}
+
+#' @rdname riskr_class
+#' @export
+as.ph <- function(x) {
+    if (missing(x)) x <- numeric()
+    if (is.ph(x)) return(x)
+    after <- match("ph", class(x), nomatch = 0L)
+    class(x) <- append(class(x), "ph", after = after)
+    x
+}
+
+#' @rdname riskr_class
+#' @export
+as.sodium <- function(x) {
+    if (missing(x)) x <- numeric()
+    if (is.sodium(x)) return(x)
+    after <- match("sodium", class(x), nomatch = 0L)
+    class(x) <- append(class(x), "sodium", after = after)
+    x
+}
+
+#' @rdname riskr_class
+#' @export
+as.potassium <- function(x) {
+    if (missing(x)) x <- numeric()
+    if (is.potassium(x)) return(x)
+    after <- match("potassium", class(x), nomatch = 0L)
+    class(x) <- append(class(x), "potassium", after = after)
+    x
+}
+
+#' @rdname riskr_class
+#' @export
+as.scr <- function(x) {
+    if (missing(x)) x <- numeric()
+    if (is.scr(x)) return(x)
+    after <- match("scr", class(x), nomatch = 0L)
+    class(x) <- append(class(x), "scr", after = after)
+    x
+}
+
+#' @rdname riskr_class
+#' @export
+as.hct <- function(x) {
+    if (missing(x)) x <- numeric()
+    if (is.hct(x)) return(x)
+    after <- match("hct", class(x), nomatch = 0L)
+    class(x) <- append(class(x), "hct", after = after)
+    x
+}
+
+#' @rdname riskr_class
+#' @export
+as.wbc <- function(x) {
+    if (missing(x)) x <- numeric()
+    if (is.wbc(x)) return(x)
+    after <- match("wbc", class(x), nomatch = 0L)
+    class(x) <- append(class(x), "wbc", after = after)
+    x
+}
+
+#' @rdname riskr_class
+#' @export
+as.gcs <- function(x) {
+    if (missing(x)) x <- numeric()
+    if (is.gcs(x)) return(x)
+    after <- match("gcs", class(x), nomatch = 0L)
+    class(x) <- append(class(x), "gcs", after = after)
+    x
+}
+
 # class test functions ---------------------------------
 
 #' Test icuriskr-related classes
@@ -85,3 +165,35 @@ is.map <- function(x) inherits(x, "map")
 #' @rdname is.riskr
 #' @export
 is.hr <- function(x) inherits(x, "hr")
+
+#' @rdname is.riskr
+#' @export
+is.rr <- function(x) inherits(x, "rr")
+
+#' @rdname is.riskr
+#' @export
+is.ph <- function(x) inherits(x, "ph")
+
+#' @rdname is.riskr
+#' @export
+is.sodium <- function(x) inherits(x, "sodium")
+
+#' @rdname is.riskr
+#' @export
+is.potassium <- function(x) inherits(x, "potassium")
+
+#' @rdname is.riskr
+#' @export
+is.scr <- function(x) inherits(x, "scr")
+
+#' @rdname is.riskr
+#' @export
+is.hct <- function(x) inherits(x, "hct")
+
+#' @rdname is.riskr
+#' @export
+is.wbc <- function(x) inherits(x, "wbc")
+
+#' @rdname is.riskr
+#' @export
+is.gcs <- function(x) inherits(x, "gcs")
