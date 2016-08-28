3f3c763ab2ed3cf73e47875aba28a1e79fc186fd
Brian Gulbis <brian.gulbis@gmail.com>
2016-08-27 21:10:02 -0500

    add package documentation file

---
+++ R/icuriskr.R
@@ -0,0 +1,13 @@
+#' Calculate ICU Morbidity and Mortality Risk Scores
+#'
+#' \pkg{icuriskr} provides methods to calculate ICU morbidity and mortality risk
+#' scores
+#'
+#' This package calculates different ICU morbidity and mortality risk scores,
+#' including APACHE II, APAPCHE III, and SAPS II. The presence of comorbidities
+#' is determined by using ICD-9-CM and ICD-10-CM codes.
+#'
+#' @docType package
+#' @name icuriskr
+#' @importFrom magrittr %>%
+NULL
