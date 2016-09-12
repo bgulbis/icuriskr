c1fd30e607751b2114d14017bce77eccb60701cb
bgulbis <brian.gulbis@gmail.com>
2016-09-12 16:06:59 -0500

    start work on function to help assgin class types

---
+++ R/helpers.R
@@ -0,0 +1,16 @@
+# helper functions
+
+#' Set class types
+#'
+#' This function takes physiologic variables in a data frame and sets them to
+#' the appropriate class types.
+#'
+#' @param df A data frame
+#' @param types A named list with column name and setter function (ex: list(hr =
+#'   as.hr))
+#'
+#' @return A data frame
+#' @keywords internal
+set_types <- function(df, types) {
+    purrr::map_df(types, ~ purrr::dmap_at(df, .at = c("hr", "sbp"), .f = .x))
+}
