9501906e9338f46c7306c3ead63ca5b6f2ee7bec
bgulbis <brian.gulbis@gmail.com>
2016-09-19 16:17:34 -0500

    convert icd9 to icd10

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -1,20 +1,20 @@
 # internal_data
 
-library(purrr)
-library(dplyr)
-library(tidyr)
+library(tidyverse)
 library(icd)
 
 comorbid_icd9 <- readr::read_csv("data-raw/comorbidities_icd9cm.csv")
 
 tmp <- comorbid_icd9 %>%
     select(-drg) %>%
-    dmap_at("icd9cm", icd_decimal_to_short) %>%
+    dmap_at("icd9cm", icd_decimal_to_short)
+
+icd9 <- tmp %>%
     slice_rows("comorbidity") %>%
     by_slice(map_at, .at = "icd9cm", .f = c)
 
-tmp2 <- flatten(tmp$.out)
-names(tmp2) <- tmp$comorbidity
+tmp2 <- flatten(icd9$.out)
+names(tmp2) <- icd9$comorbidity
 
 tmp3 <- tmp2 %>%
     map(as.icd9) %>%
@@ -31,6 +31,18 @@ comorbidity_map_icd9 <- as.icd_comorbidity_map(tmp3)
 comorbidity_drg <- comorbid_icd9 %>%
     distinct(comorbidity, drg) %>%
     filter(!is.na(drg))
+
+cols <- fwf_empty("data-raw/2016_I9gem.txt", col_names = c("icd9", "icd10", "other"))
+icd10 <- read_fwf("data-raw/2016_I9gem.txt", cols) %>%
+    filter(icd10 != "NoDx")
+
+tmp4 <- inner_join(tmp, icd10, c("icd9cm" = "icd9")) %>%
+    select(-icd9cm, -other) %>%
+    slice_rows("comorbidity") %>%
+    by_slice(map_at, .at = "icd10", .f = c)
+
+tmp5 <- flatten(tmp4$.out)
+names(tmp5) <- tmp4$comorbidity
 
 devtools::use_data(comorbidity_map_icd9, comorbidity_drg, overwrite = TRUE)
 
