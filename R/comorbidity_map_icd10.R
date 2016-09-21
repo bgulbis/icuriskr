56db4a3e9fbc59b3491bc5edd65cbd268efd2c1d
bgulbis <brian.gulbis@gmail.com>
2016-09-21 11:49:17 -0500

    update comorbidity mapping documentation

--- R/comorbidity_map_icd10.R
+++ R/comorbidity_map_icd10.R
@@ -1,9 +1,20 @@
 #' Comorbidity mapping using ICD-10-CM
 #'
 #' A list containing the ICD-10-CM codes for comorbidities included in APAPCHE
-#' II, APAPCHE III, and SAPS II.
+#' II, APAPCHE III, and SAPS II. Based on manual identification diagnosis codes
+#' for most comorbidities.
 #'
 #' @format A named list with a character vector of ICD-10-CM codes which
 #'   correspond to the comorbidities.
 #'
-"comorbidity_map_icd10"
+"comorbidity_map_icd10_ek"
+
+#' Comorbidity mapping using ICD-10-CM
+#'
+#' A list containing the ICD-10-CM codes for comorbidities included in APAPCHE
+#' II, APAPCHE III, and SAPS II. Based on original Elixhauser list.
+#'
+#' @format A named list with a character vector of ICD-10-CM codes which
+#'   correspond to the comorbidities.
+#'
+"comorbidity_map_icd10_elix"
